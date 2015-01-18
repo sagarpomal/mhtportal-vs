4552fb8fd4e375ecc95387435231808ecb64a060
Sagar Pomal <sagarpomal@gmail.com>
2015-01-19 00:09:49 +0530

    Done

---
+++ DjangoWebProject2/app/models.py
@@ -0,0 +1,14 @@
+"""
+Definition of models.
+"""
+
+from django.db import models
+
+
+# Create your models here.
+
+
+
+
+
+    
---
+++ DjangoWebProject2/mhtportal_env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\sagar\documents\visual studio 2013\Projects\DjangoWebProject2\DjangoWebProject2\mhtportal_env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
